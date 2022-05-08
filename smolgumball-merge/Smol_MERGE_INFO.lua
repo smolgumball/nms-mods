1f8d483fa7e0c0bf4e6aca06a21833f80ce39666
smolgumball <smolgumball@pm.me>
2022-05-08 12:04:43 -0600

    add mod version

--- smolgumball-merge/Smol_MERGE_INFO.lua
+++ smolgumball-merge/Smol_MERGE_INFO.lua
@@ -1,8 +1,8 @@
 GAME_VERSION = "3.89"
+MOD_VERSION = "v1"
 
 NMS_MOD_DEFINITION_CONTAINER = {
-	["MOD_FILENAME"] 		= "smolgumball-merge__" .. GAME_VERSION .. ".pak",
-	["MOD_BATCHNAME"]		= "smolgumball-merge__" .. GAME_VERSION .. ".pak",
+	["MOD_BATCHNAME"]		= "smolgumball-merge__" .. GAME_VERSION .. "-" .. MOD_VERSION .. ".pak",
 	["MOD_DESCRIPTION"]		= "Big ol' merge",
 	["MOD_AUTHOR"]			= "smolgumball",
 	["NMS_VERSION"]			= GAME_VERSION,
