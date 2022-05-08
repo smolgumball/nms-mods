0cb27ca95c522b29d333c91c74afabc4d9cbd806
smolgumball <smolgumball@pm.me>
2022-05-08 11:55:25 -0600

    Update merge info

--- smolgumball-merge/Smol_MERGE_INFO.lua
+++ smolgumball-merge/Smol_MERGE_INFO.lua
@@ -1,9 +1,11 @@
+GAME_VERSION = "3.89"
+
 NMS_MOD_DEFINITION_CONTAINER = {
-	["MOD_FILENAME"] 		= "smolgumball-merge.pak",
-	["MOD_BATCHNAME"]		= "smolgumball-merge.pak",
+	["MOD_FILENAME"] 		= "smolgumball-merge__" .. GAME_VERSION .. ".pak",
+	["MOD_BATCHNAME"]		= "smolgumball-merge__" .. GAME_VERSION .. ".pak",
 	["MOD_DESCRIPTION"]		= "Big ol' merge",
 	["MOD_AUTHOR"]			= "smolgumball",
-	["NMS_VERSION"]			= "3.89",
+	["NMS_VERSION"]			= GAME_VERSION,
 	["MODIFICATIONS"] 		= {
 		{
 			["MBIN_CHANGE_TABLE"] = {
